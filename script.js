c017dcddbcc6ff2e7ce84555902035beafd1c536
Clean dev <92629860+clean-dev-com@users.noreply.github.com>
2023-06-05 20:10:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// var splitText = new SplitText('.h6', { type: 'words, chars' });
+// var splitText2 = new SplitText('.h1', { type: 'words, chars' });
+// var splitText3 = new SplitText('.h4', { type: 'words, chars' });
+// var splitText3 = new SplitText('.p', { type: 'words, chars' });
+
+// TweenMax.staggerFrom(splitText.chars, 1, {
+//    y: -50,
+//    opacity: 0,
+//    stagger: .05,
+//    ease: 'back'
+// })
+
+// TweenMax.staggerFrom(splitText2.chars, 1, {
+//    delay: .8,
+//    y: -50,
+//    opacity: 0,
+//    stagger: .05,
+//    ease: 'back'
+// })
+
+// TweenMax.staggerFrom(splitText3.chars, 1, {
+//    delay: 1.6,
+//    y: -50,
+//    opacity: 0,
+//    stagger: .05,
+//    ease: 'back'
+// })
+
+// TweenMax.staggerFrom(splitText3.chars, 1, {
+//    delay: 2.4,
+//    y: -50,
+//    opacity: 0,
+//    stagger: .01,
+//    ease: 'back'
+// })
